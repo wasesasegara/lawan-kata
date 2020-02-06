c06bf40f770c798bd4c34f7799691355bc7c4766
Bisma S Wasesasegara <bisma.s@pegipegi.com>
2020-02-06 16:32:04 +0700

    Init project

---
+++ Lawan Kata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lawan Kata
+//
+//  Created by Bisma S Wasesasegara on 06/02/20.
+//  Copyright © 2020 Bisma S Wasesasegara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
