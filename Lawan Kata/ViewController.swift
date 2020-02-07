b4572e57829b1aab14011ac1a949aefa71caea22
Bisma S Wasesasegara <bisma.s@pegipegi.com>
2020-02-07 16:14:08 +0700

    Add gesture touches and dummy logic

--- Lawan Kata/ViewController.swift
+++ Lawan Kata/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private var dif: CGFloat = 0
+    private var origin: CGFloat = 0
+    private var scale: CGFloat = 1
+    private var width: CGFloat = 128
+    private var height: CGFloat = 128
+    
     @IBOutlet weak var viewObject: UIView!
     @IBOutlet weak var segmentSwitch: UISegmentedControl!
     
@@ -29,6 +35,39 @@ final class ViewController: UIViewController {
         guard let object = viewObject else { return }
         object.center = CGPoint(x: view.center.x, y: view.height() - Constants.bottomPoint - object.height() / 2)
     }
+    
+    private func setupSegment() { }
+    
+    private func refreshRect() {
+        guard let object = viewObject else { return }
+        view.layer.removeAllAnimations()
+        width += dif > 0 ? 10 : -10
+        height += dif > 0 ? 10 : -10
+        if width > 300 || height > 300 {
+            width = 300
+            height = 300
+        } else if width < 50 || height < 50 {
+            width = 50
+            height = 50
+        }
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: {
+            object.frame = CGRect(x: 0, y: 0, width: self.width, height: self.height)
+            self.moveToBottomCenter()
+        }) { (_) in
+            
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let originTouch = touches.first?.location(in: self.view) else { return }
+        origin = originTouch.y
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let originTouch = touches.first?.location(in: self.view) else { return }
+        dif = origin - originTouch.y
+        refreshRect()
+        origin = originTouch.y
+    }
 
 }
-
